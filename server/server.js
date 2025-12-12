6630563117647e1a305498a9f6d0a897f29ce4e1
ULRIKE KUNZE <ULRIKE.E.KUNZE@GMAIL.COM>
2025-12-12 09:37:55 +0000

    chore: added GET and POST methods to server.js file

--- server/server.js
+++ server/server.js
@@ -21,3 +21,30 @@ app.get("/", (req, res) => {
 //==============================================initial set up finished===============================================
 
 //==============================================Getting Data from the client==========================================
+
+app.post("new-skylog", (req, res) => {
+  const newSkyLog = req.body.formValues;
+  console.log(newSkyLog);
+  const query = db.query(
+    `INSERT INTO skylog (username, date, departure, arrival, airline, aircraft) VALUES ($1, $2, $3, $4, $5, $6)`,
+    [
+      newSkyLog.username,
+      newSkyLog.date,
+      newSkyLog.departure,
+      newSkyLog.arrival,
+      newSkyLog.airline,
+      newSkyLog.aircraft,
+    ]
+  );
+  res.json({ status: "success, values: newSkyLog" });
+});
+
+import { db } from "./dbConnection.js";
+
+app.get("/skylog", async function (request, response) {
+  const query = await db.query(
+    `SELECT username, flight_date, departure, arrival, airline, aircraft FROM skylog; `
+  );
+  console.log(query);
+  response.json(query.rows);
+});
